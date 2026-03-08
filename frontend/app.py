92dfcee031c0c5b96752bdef413a9de908a5046b
Alvin Sudhan <alvinofficial646@gmail.com>
2026-03-08 21:53:35 +0530

    connect frontend to hosted backend

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 import os
-API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
+API_URL = os.getenv("API_URL", "https://ai-for-bharat-7.onrender.com")
 
 st.set_page_config(page_title="Sahayak AI", layout="centered")
 
